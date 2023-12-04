9f87de8bc0e0e330252a5adee8e984b387c95f27
shivam bhargava <shivambhargava618@gmail.com>
2023-12-04 12:56:30 +0530

    chat check

--- src/Pages/Account/UpdateEmail.js
+++ src/Pages/Account/UpdateEmail.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SettingModal from '../../modal/Setting.modal';
 import { helper } from '../../lib/helper';
 
@@ -6,8 +6,19 @@ const UpdateEmail = () => {
   const urlParams = new URLSearchParams(window.location.search);
   const token = urlParams.get('token');
   console.log('Token:', token);
-  const emailverify = () => {
-    SettingModal.UpdateEmail({ token }).then((res) => {
+  const [formData, setformData] = useState({
+    email: '',
+})
+const handleInputChange1 = (event) => {
+    setformData({
+        ...formData,
+        [event.target.name]: event.target.value,
+    });
+};
+  const emailverify = (event) => {
+    event.preventDefault();
+    const form = new FormData(event.target);
+    SettingModal.UpdateEmail( token, form ).then((res) => {
 
          helper.sweetalert.toast("Your Email is Verifiyed")
          window.location.replace("/user/Settingd")
@@ -19,9 +30,10 @@ const UpdateEmail = () => {
   return (
     <div>
       <h4>UPDATE EMAIL</h4>
-      <form>
+      <form onSubmit={emailverify}>
         <label htmlFor="">Update Email</label>
         <input type="text" name="email" placeholder='Enter your Update Email' id="" />
+        <button>submit</button>
       </form>
     </div>
   )
